21c9a794656e1d7376063c918c2f7d7cd44e22b1
Brian Schrock <bschrock@manta.com>
2015-07-08 10:42:57 -0400

    Cleanup and made some things faster and easier to read.

--- lib/aws-cache.rb
+++ lib/aws-cache.rb
@@ -21,16 +21,16 @@ class AwsCache
     @region = optional_element(opts, ['region'], 'us-east-1')
   end
 
+  #Returns a hash describing the instance requested.
   def describe_instance( instance_id)
     instances = self.describe_instances()
-    instances.each do |instance|
-      if instance[:instances][0][:instance_id] == instance_id then
-        return instance[:instances][0]
-      end
+    instance = instances.select do |entry|
+      entry[:instances][0][:instance_id] == instance_id
     end
-    return nil
+    return instance[0]
   end
 
+  #Returns an array of hashes describing the autoscaling groups for the selected stack.
   def stack_auto_scaling_groups(stack_name)
     autoscaling_groups = Array.new()
     output = self.list_stack_resources(stack_name)
@@ -42,16 +42,7 @@ class AwsCache
     return autoscaling_groups
   end
 
-  def list_stack_resources( stack_name)
-    output = cache_get_2("list_stack_resources-#{stack_name}", 300) do
-      aws_object = Aws::CloudFormation::Client.new(region: @region)
-      pages = aws_object.list_stack_resources(stack_name: stack_name)
-      output = process_page( 'stack_resource_summaries', pages)
-    end
-    return output
-  end
-
-
+  #Returns an array of hashes describing the substacks for the selected stack.
   def get_sub_stacks( stack_name)
     substacks = Array.new()
     stacks = self.list_stack_resources(stack_name)
@@ -66,7 +57,8 @@ class AwsCache
     end
     return substacks
   end
-        
+  
+  #Returns a hash describing the stack requested.      
   def describe_stack(stack_name)
     stacks = self.describe_stacks
     stacks.each do |stack|
@@ -77,17 +69,30 @@ class AwsCache
     return nil
   end
 
-  def describe_stacks()
-    output = cache_get_2('get_stacks', 300) do
-      aws_object = Aws::CloudFormation::Client.new(region: @region)
-      pages = aws_object.describe_stacks
-      output = process_page( 'stacks', pages)
+  #Returns an array of hashes of instances.
+  def get_asg_instances(asg)
+    instances = Array.new()
+    asg_instances = self.describe_auto_scaling_group(asg)
+    asg_instances[:instances].each do |instance|
+      instances.push(instance)
     end
-    return output
+    return instances
+  end
+
+  #Returns Aws::AutoScaling::Types::AutoScalingGroup
+  def describe_auto_scaling_group(asg)
+    asgroups = self.describe_autoscaling_groups()
+    target_asg = asgroups.select do |record|
+      record[:auto_scaling_group_name] == asg
+    end
+    if target_asg then
+      return target_asg[0]
+    end
+    return nil
   end
 
   def describe_snapshots()
-    output = cache_get_2('get_snapshots', 300) do
+    output = cache_get('get_snapshots', 300) do
       aws_object = Aws::EC2::Client.new(region: @region)
       pages = aws_object.describe_snapshots
       output = process_page( 'snapshots', pages)
@@ -95,27 +100,38 @@ class AwsCache
     return output
   end
 
-  def get_asg_instances(asg)
-    instances = Array.new()
-    asg = self.describe_auto_scaling_group(asg)
-    asg[:instances].each do |instance|
-      instances.push(instance)
+  def list_stack_resources( stack_name)
+    output = cache_get("list_stack_resources-#{stack_name}", 300) do
+      aws_object = Aws::CloudFormation::Client.new(region: @region)
+      pages = aws_object.list_stack_resources(stack_name: stack_name)
+      output = process_page( 'stack_resource_summaries', pages)
     end
-    return instances
+    return output
   end
 
-  def describe_auto_scaling_group(asg)
-    asgroups = self.describe_autoscaling_groups()
-    asgroups.each do |asgroup|
-      if asgroup[:auto_scaling_group_name] == asg then
-        return asgroup
-      end
+  def describe_stacks()
+    output = cache_get('get_stacks', 300) do
+      aws_object = Aws::CloudFormation::Client.new(region: @region)
+      pages = aws_object.describe_stacks
+      output = process_page( 'stacks', pages)
     end
-    return nil
+    return output
   end
 
+
+  def describe_auto_scaling_instances()
+    output = cache_get("describe_auto_scaling_instances", 300) do
+      aws_object = Aws::AutoScaling::Client.new(region: @region)
+      pages = aws_object.describe_auto_scaling_instances()
+      output = process_page( 'auto_scaling_instances', pages)
+    end
+    return output
+  end
+
+
+  #Returns an Array of Hashes containing auto scaling group structures
   def describe_autoscaling_groups()
-    output = cache_get_2('get_autoscaling_groups', 300) do
+    output = cache_get('get_autoscaling_groups', 300) do
       aws_object = Aws::AutoScaling::Client.new(region: @region) 
       pages = aws_object.describe_auto_scaling_groups
       output = process_page( 'auto_scaling_groups', pages)
@@ -123,8 +139,9 @@ class AwsCache
     return output
   end
 
+  #Returns an Array of Hashes containing instance structures
   def describe_instances()
-    output = cache_get_2('describe_instances', 300) do
+    output = cache_get('describe_instances', 300) do
       aws_object = Aws::EC2::Client.new(region: @region)
       pages = aws_object.describe_instances
       output = process_page( 'reservations', pages)
@@ -135,10 +152,9 @@ class AwsCache
   def process_page( key, pages)
     output = Array.new()
     pages.each do |page|
-      page.each do |data|
-        data.data[key].each do |entry|
-          output.push(entry)
-        end
+      data = page.send( key)
+      data.each do |asg|
+        output.push(asg)
       end
     end
     return output
@@ -166,36 +182,17 @@ class AwsCache
     return false
   end
 
-  def cache_get_2(key, ttl)
+  def cache_get(key, ttl)
     vkey = "#{key}_#{@keyspace}"
     output = @redis.get(vkey)
     if output.nil?
       output = yield
-      output = YAML.dump(output)
+      output = Psych.dump(output)
       @redis.setex(vkey, ttl, output )
     end
-    return YAML.load(output)
+    return Psych.load(output)
   end
 
-
-  def cache_get(key, ttl)
-    vkey = "#{key}_#{@keyspace}"
-    hash = @redis.get(vkey)
-    ap hash.class.name
-    unless hash.nil?
-      hash = YAML.load(hash)
-    end
-
-    if hash.nil?
-      hash = yield
-
-      hash = hash.to_yaml
-      @redis.setex(vkey, ttl, hash)
-      hash = YAML.load(hash)
-    end
-
-    return hash
-  end
 
   def list_to_hash!(src, keys, by_key)
     hash = {}
